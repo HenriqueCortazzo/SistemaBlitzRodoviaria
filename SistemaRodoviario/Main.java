44e5015a765e38d128628a5516fedef4a5d8b137
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-06 06:39:01 -0300

    Instânciando Classe RandomVeiculos na Classe Main.

--- SistemaRodoviario/Main.java
+++ SistemaRodoviario/Main.java
@@ -2,6 +2,7 @@ package SistemaRodoviario;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        RandomVeiculos rd = new RandomVeiculos();
+        rd.iniciarSistema();
     }
 }
