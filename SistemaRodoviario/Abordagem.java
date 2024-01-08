465068815451e58fdc624c90477a0c511571002b
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-08 05:40:41 -0300

    Implementação método relatorio

--- SistemaRodoviario/Abordagem.java
+++ SistemaRodoviario/Abordagem.java
@@ -50,33 +50,33 @@ public class Abordagem {
                 String portaDocumentos = scanner.next();
                 double debitosVeiculo = licenciamento + ipva;
                 Situacao situacao = new Situacao();
-                situacao.relatorio();
+               // situacao.relatorio();
                 if (habilitacao.equalsIgnoreCase("s") && portaHabilitacao.equalsIgnoreCase("s") && portaDocumentos.equalsIgnoreCase("s") && debitosVeiculo == 0) {
                     veiculoRegular = true;
                     System.out.println(veiculoRegular);
-                    situacao.relatorio();
+                    situacao.relatorio(tipoVeiculo,placaVeiculo,ipva,licenciamento,habilitacao,portaHabilitacao,portaDocumentos,debitosVeiculo);
                     //relatorio
                 } else if (!habilitacao.equalsIgnoreCase("s") && !portaHabilitacao.equalsIgnoreCase("s") && portaDocumentos.equalsIgnoreCase("s") && debitosVeiculo == 0) {
                     veiculoRegular = false;
                     System.out.println(veiculoRegular);
-                    situacao.relatorio();
+                   // situacao.relatorio();
                     //relatorio
                     //patio
                 } else if (habilitacao.equalsIgnoreCase("s") && !portaHabilitacao.equalsIgnoreCase("s") && portaDocumentos.equalsIgnoreCase("s") && debitosVeiculo == 0) {
                     veiculoRegular = false;
                     System.out.println(veiculoRegular);
-                    situacao.relatorio();
+                   // situacao.relatorio();
                     //relatorio
                     //multa
                 } else if (habilitacao.equalsIgnoreCase("s") && portaHabilitacao.equalsIgnoreCase("s") && !portaDocumentos.equalsIgnoreCase("s") && debitosVeiculo == 0) {
                     veiculoRegular = true;
                     System.out.println(veiculoRegular);
-                    situacao.relatorio();
+                   // situacao.relatorio();
                     //relatorio
                 } else if (debitosVeiculo != 0) {
                     veiculoRegular = false;
                     System.out.println(veiculoRegular);
-                    situacao.relatorio();
+                   // situacao.relatorio();
                     //relatorio
                     //patio
                 }
--- SistemaRodoviario/Situacao.java
+++ SistemaRodoviario/Situacao.java
@@ -1,9 +1,31 @@
 package SistemaRodoviario;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Situacao {
 
-    public  void  relatorio(){
-
+    public void relatorio(String tipo, String placa, double ipva, double licenciamento, String habilitacao, String pHabilitacao, String pDocumentos, double debitosTotais) {
+        Abordagem abordagem = new Abordagem();
+        System.out.println("\nTIPO DO VEÍCULO: " + tipo.toUpperCase());
+        System.out.println("PLACA DO VEÍCULO: " + placa.toUpperCase());
+        System.out.printf("VALOR IPVA EM ABERTO: R$ %.2f\n",ipva);
+        System.out.printf("VALOR LICENCIAMENTO EM ABERTO: R$ %.2f\n", licenciamento);
+        if (habilitacao.equalsIgnoreCase("s")) {
+            System.out.println("HABILITAÇÃO: OK");
+        } else {
+            System.out.println("HABILITAÇÃO: NADA CONSTA");
+        }
+        if (pHabilitacao.equalsIgnoreCase("s")) {
+            System.out.println("CIDADÃO PORTA HABILITAÇÃO: OK");
+        } else {
+            System.out.println("CIDADÃO PORTA HABILITAÇÃO: NADA CONSTA");
+        }
+        if (pDocumentos.equalsIgnoreCase("s")) {
+            System.out.println("CIDADÃO PORTA DOCUMENTOS: OK");
+        } else {
+            System.out.println("CIDADÃO PORTA DOCUMENTOS: NADA CONSTA");
+        }
+        System.out.printf("DÉBITOS TOTAIS: R$ %.2f\n", debitosTotais);
     }
 
 }
