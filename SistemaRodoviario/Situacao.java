887c7f44b8cf9deea72500a2b64cf628f4eb7637
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-09 06:25:03 -0300

    Adicionando loops para saída formatada

--- SistemaRodoviario/Situacao.java
+++ SistemaRodoviario/Situacao.java
@@ -2,7 +2,7 @@ package SistemaRodoviario;
 
 public class Situacao {
 
-    public static String[] veiculosLiberados = new String[10];
+    public static String[] veiculosLiberados = new String[Patio.veiculosLiberados];
     public static String[] veiculosApreendidos = new String[10];
 
     public void relatorio(double multa, long renavam, boolean sitVeicular, String tipo, String marca, String nome, String ano, String placa, double ipva, double licenciamento, String habilitacao, String pHabilitacao, String pDocumentos, double debitosTotais) {
@@ -37,13 +37,15 @@ public class Situacao {
             System.out.println("\nVEÍCULO LIBERADO.");
             Patio.veiculosLiberados++;
             for (int i = 0; i < veiculosLiberados.length; i++) {
-                    veiculosLiberados[i] ="PLACA: "+placa+" | RENAVAM:"+renavam;
+                if (veiculosLiberados[i].equalsIgnoreCase(null)) {
+                    veiculosLiberados[i] = "PLACA: " + placa + " | RENAVAM:" + renavam;
+                }
             }
         } else {
             System.out.println("\nO VEÍCULO SERÁ DESTINADO AO PÁTIO MAIS PROXIMO.\n");
             Patio.veiculosApreendidos++;
             for (int i = 0; i < veiculosApreendidos.length; i++) {
-                veiculosApreendidos[i] ="PLACA: "+placa+" | RENAVAM:"+renavam;
+                veiculosApreendidos[i] = "PLACA: " + placa + " | RENAVAM:" + renavam;
             }
             patio.retirarVeiculo(debitosTotais, placa, renavam);
         }
