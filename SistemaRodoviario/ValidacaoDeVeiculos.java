15afdf92f46da0dd164c58b18b149ad36495b3c8
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-20 10:22:44 -0300

    Inicializando tipo de veiculos

--- SistemaRodoviario/ValidacaoDeVeiculos.java
+++ SistemaRodoviario/ValidacaoDeVeiculos.java
@@ -1,5 +1,5 @@
 package SistemaRodoviario;
 
 public class ValidacaoDeVeiculos {
-    
+    private String[] tipoVeiculos = {"Carro", "Moto", "Quadriciclo", "Microônibus", "Ônibus", "Reboque", "Caminhão", "Caminhonete", "Trator", "Guindaste", "Munck"};
 }
