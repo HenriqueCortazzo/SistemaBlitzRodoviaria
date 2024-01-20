55363e9ff961aed08c85cb8093c2d9f0e402cef0
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-20 10:32:42 -0300

    Inicialização tipos de carro

--- SistemaRodoviario/ValidacaoDeVeiculos.java
+++ SistemaRodoviario/ValidacaoDeVeiculos.java
@@ -1,5 +1,7 @@
 package SistemaRodoviario;
 
 public class ValidacaoDeVeiculos {
-    private String[] tipoVeiculos = {"Carro", "Moto", "Quadriciclo", "Microônibus", "Ônibus", "Reboque", "Caminhão", "Caminhonete", "Trator", "Guindaste", "Munck"};
+    private final String[] tipoVeiculos = {"Carro", "Moto", "Quadriciclo", "Microônibus", "Ônibus", "Reboque", "Caminhão", "Caminhonete", "Trator", "Guindaste", "Munck"};
+
+    private final String[] marcaCarros = {"Audi", "BMW", "Chevrolet", "Dodge", "FIAT", "Ferrari", "Ford", "Honda", "Jeep", "Kia", "Lamborghini", "Land Rover", "Mercedes", "Nissan", "Peugeot", "Porsche", "Renault", "Toyota", "Volkswagen"};
 }
