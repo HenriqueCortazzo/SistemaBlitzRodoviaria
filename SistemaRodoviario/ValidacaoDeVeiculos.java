caecc64677bd59d5bada2ed5a7591ffc25e9b934
HenriqueCortazzo <qdeusmeajude@gmail.com>
2024-01-21 16:37:45 -0300

    Implementação do método validarVeiculo()

--- SistemaRodoviario/ValidacaoDeVeiculos.java
+++ SistemaRodoviario/ValidacaoDeVeiculos.java
@@ -7,15 +7,31 @@ public class ValidacaoDeVeiculos {
 
     private final String[] marcaCarros = {"Audi", "BMW", "Chevrolet", "Dodge", "FIAT", "Ferrari", "Ford", "Honda", "Jeep", "Kia", "Lamborghini", "Land Rover", "Mercedes", "Nissan", "Peugeot", "Porsche", "Renault", "Toyota", "Volkswagen"};
 
+    private String tipoCarroSave = "";
+
+    private boolean veiculoValidado = false;
+    Scanner scanner = new Scanner(System.in);
+
     public void validarVeiculo(String tipo) {
         for (String validarVeiculo : tipoVeiculos) {
+            if (validarVeiculo.equalsIgnoreCase(tipo)) {
+                tipoCarroSave = validarVeiculo;
+                veiculoValidado = true;
+                break;
+            } else veiculoValidado = false;
+        }
+        if (!veiculoValidado) {
             do {
                 System.out.println("Esse tipo de veiculo não é aceitável");
                 System.out.println("Digite outro tipo: ");
-                Scanner scanner = new Scanner(System.in);
                 tipo = scanner.next();
-            } while (validarVeiculo.equalsIgnoreCase(tipo));
+                validarVeiculo(tipo);
+            } while (tipoCarroSave.equalsIgnoreCase(""));
         }
     }
 
+    public String getTipoCarroSave() {
+        return tipoCarroSave;
+    }
 }
+
